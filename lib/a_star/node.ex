5c496793e8b58bc7f598b0eabf37a2fbdeebb931
William Fank Thomé <willegp88@gmail.com>
2021-09-19 13:07:21 -0300

    feat: node f_score

--- lib/a_star/node.ex
+++ lib/a_star/node.ex
@@ -14,4 +14,19 @@ defmodule AStar.Node do
   """
   def distance(%__MODULE__{} = n1, %__MODULE__{} = n2),
     do: Point.distance(n1.point, n2.point)
+
+  @spec f_score(t, t, t) :: number()
+
+  @doc """
+  Represents the best guess to how short a path from start to finish can be.
+  """
+  def f_score(
+        %__MODULE__{} = current,
+        %__MODULE__{} = next,
+        %__MODULE__{} = destination
+      ) do
+    g_score = distance(current, next)
+    h = distance(next, destination)
+    g_score + h
+  end
 end
--- test/a_star/node_test.exs
+++ test/a_star/node_test.exs
@@ -12,4 +12,15 @@ defmodule AStar.NodeTest do
 
     assert result == expected_result
   end
+
+  test "f_score/2 returns the sum of two distances (g_score and h)" do
+    current = %Node{point: %Point{x: 0}}
+    next = %Node{point: %Point{x: 1}}
+    destination = %Node{point: %Point{x: 2}}
+
+    result = Node.f_score(current, next, destination)
+    expected_result = 2
+
+    assert result == expected_result
+  end
 end
